9a43c1291f12c38e6b68ef9dee4be1bae04bb874
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-12-04 18:00:19 +0700

    create artists detail

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,34 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { DetailsHeader, Loader, RelatedSongs } from "../components";
 
+const ArtistDetails = () => {
+  const dispath = useDispatch();
+  const { id: artistId } = useParams();
+
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const {
+    data: artistData,
+    isFetching: isFetchingArtist,
+    error,
+  } = useGetArtistDetailsQuery({ artistId });
+
+  if (isFetchingArtist) return <Loader title="Searching artist details" />;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistId} artistData={artistData} />
+
+      <RelatedSongs
+        data={Object.values(artistData?.songs)}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
 export default ArtistDetails;
